5475595087ee42eeab73216e39f7d5b5a170a102
sidewinder2020 <sdwinder1@gmail.com>
2017-11-27 01:46:12 -0700

    bubble sort

--- app/bubble_sort.js
+++ app/bubble_sort.js
@@ -1,3 +1,13 @@
 exports.sort = function(array) {
-  return [2, 3, 6, 7, 10];
-}
+  for (var j = 0; j < array.length; j++) {
+    for (var i = 0; i < array.length - 1; i++) {
+      if (array[i] > array[i + 1]) {
+        var current = array[i];
+        var next = array[i + 1];
+        array[i] = next;
+        array[i + 1] = current;
+      };
+    };
+  };
+  return array;
+};
