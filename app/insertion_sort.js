c0450a807527b8258d1b68ed24c4a4ec25340fce
sidewinder2020 <sdwinder1@gmail.com>
2017-11-27 01:09:38 -0700

    insertion sort - it isn't pretty, but it's my brother

--- app/insertion_sort.js
+++ app/insertion_sort.js
@@ -1,3 +1,24 @@
 exports.sort = function(array) {
-  return [2, 3, 6, 7, 10];
-}
+  var newArray = [];
+  newArray.unshift(array.shift());
+  var i = 0;
+
+  while (array.length != 0) {
+    if (array[0] < newArray[0]) {
+      newArray.unshift(array[0]);
+      array.shift();
+    }
+    else if (array[0] > newArray[newArray.length - 1]) {
+      newArray.push(array[0]);
+      array.shift();
+    }
+    else if (array[0] > newArray[i]) {
+      newArray.splice(i + 1, 0, array[0]);
+      array.shift();
+    }
+    else {
+      i++
+    }
+  }
+  return newArray;
+};
